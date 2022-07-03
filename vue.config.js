be9f671e08122e4e991cda091bfb4a0353e11f5e
jyb <3257223409@@qq.com>
2022-07-03 10:43:21 +0800

    '项目目录结构初始化'

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
+// const path = require('path')
+
 const { defineConfig } = require('@vue/cli-service')
+// defineConfig:提示语法
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 关闭eslint, build serve将不会进行eslint检查
+  lintOnSave: false, // 关闭语法检查
+
+  // 写原生webpack的配置项
+  configureWebpack: {
+    devServer: {
+      open: true,
+      port: 3000,
+      host: 'localhost'
+    }
+    //  // 解析路径
+    // resolve: {
+    //   alias: {
+    //     '#': path.join(__dirname, 'src/components')
+    //   }
+    // }
+  }
 })
